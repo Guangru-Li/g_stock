6a8e5c43516dfb8676d42e6af311ef11af3f72e2
Guangru-Li <grlee.pku@gmail.com>
2024-05-02 23:34:22 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='moduleofG',
+    version='0.1.0',
+    author='G--R Li',
+    author_email='grlee.pku@gmail.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
