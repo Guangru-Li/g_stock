0758dceca4d4d8c5f59439e086a28a1ff5b606b3
Guangru-Li <grlee.pku@gmail.com>
2024-05-29 09:53:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='g_stock',
-    version='0.1.0',
+    version='0.1.1',
     author='G--R Li',
     author_email='grlee.pku@gmail.com',
     url='https://github.com/MichaelKim0407/tutorial-pip-package',
